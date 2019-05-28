156777e6ea1e27dad20c99eb3841ce4c38b4f4bc
epgtne <epgtne@w0mba7.local>
2019-05-28 15:48:16 +0900

    update

--- etc/init/brew.sh
+++ etc/init/brew.sh
@@ -1,11 +1,47 @@
 #!/usr/bin/env bash
 
 # Install command-line tools using Homebrew.
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if test ! "$( command -v brew )"; then
+    echo "Installing homebrew"
+    /usr/bin/ruby -e "$( curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install )"
+fi
+# full-path or ruby command
 
-# Make sure we’re using the latest Homebrew.
-brew update
+echo -e "\\n\\nInstalling homebrew packages..."
+echo "=============================="
 
-# Upgrade any already-installed formulae.
-brew upgrade
+formulae={
+    vim
+    git
+    tmux
+    reattach-to-user-namespace
+    fzf
+    tree
+    zsh
+    python
+    zplug
+}
+
+# After the install, setup fzf
+echo -e "\\n\\nRunning fzf install script..."
+echo "=============================="
+/usr/local/opt/fzf/install --all --no-bash --no-fish
+
+# after the install, install neovim python libraries
+echo -e "\\n\\nRunning Neovim Python install"
+echo "=============================="
+pip2 install --user neovim
+pip3 install --user neovim
+
+# Change the default shell to zsh
+zsh_path="$( command -v zsh )"
+if ! grep "$zsh_path" /etc/shells; then
+    echo "adding $zsh_path to /etc/shells"
+    echo "$zsh_path" | sudo tee -a /etc/shells
+fi
+
+if [[ "$SHELL" != "$zsh_path" ]]; then
+    chsh -s "$zsh_path"
+    echo "default shell changed to $zsh_path"
+fi
 
