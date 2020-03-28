2e26316b8a36e9726b3e83511c343df580422fe9
kabn3d <hello0183nbq@gmail.com>
2020-03-28 17:19:35 +0900

    update

---
+++ src/init/apt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade
+
+# install Guest Additions
+sudo apt install gcc make perl linux-headers-$(uname -r)
+
+yes | sudo apt install git
+yes | sudo apt install vim
+yes | sudo apt install zsh
+
+chsh -s $(which zsh)
