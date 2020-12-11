f19ec8d72a1fca1059f625e9fe063c407f168088
kabn3d <44823403+kabn3d@users.noreply.github.com>
2020-12-11 22:04:58 +0900

    Update and rename apt.sh to deb.sh

--- src/init/deb.sh
+++ src/init/deb.sh
@@ -9,5 +9,6 @@ sudo apt install gcc make perl linux-headers-$(uname -r)
 yes | sudo apt install git
 yes | sudo apt install vim
 yes | sudo apt install zsh
+yes | sudo apt install tmux
 
 chsh -s $(which zsh)
