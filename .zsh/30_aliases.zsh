de87a95310972713c0ec5622f2c1359eb908bfce
kabn3d <44823403+kabn3d@users.noreply.github.com>
2020-12-11 21:53:38 +0900

    Update 30_aliases.zsh

--- .zsh/30_aliases.zsh
+++ .zsh/30_aliases.zsh
@@ -1,14 +1,7 @@
-alias ..='cd ..'
-alias ...='cd ../..'
-alias ....='cd ../../..'
-alias .....='cd ../../../..'
-
-alias ls="ls ${colorflag}"  # Always use color output for 'ls'
-alias l="ls -lAhF"          # Show hidden all files
-alias ll="ls -lFh"          # Show all files in long format
-alias lld="ls -l | grep ^d" # Show only directories
-
 # Helpers
+alias ls="ls ${colorflag}"  # Always use color output for 'ls'
+alias ll="ls -lFh"          # Show all files in long format
+
 alias grep='grep --color=auto'
 alias egrep='egrep --color=auto'
 alias fgrep='fgrep --color=auto'
@@ -16,12 +9,8 @@ alias fgrep='fgrep --color=auto'
 # tree
 # alias tree = 'tree -a -I "\.DS_Store|\.git|node_modules|vendor\/bundle" -N'
 
-# History
-alias hs='history -E'
-alias hsg='history -E 1| grep'
-
 # vim
-alias v='vim'
+alias vi='vim'
 alias nvim='vim -N -u NONE -i NON' # Use plain vim
 
 if ! (command -v vim) > /dev/null 2>&1; then
@@ -29,31 +18,10 @@ if ! (command -v vim) > /dev/null 2>&1; then
 fi
 
 # etc
-alias q="exit"
-
-alias g="git"
-alias tx="tmux"
-
 alias sudo='sudo ' # Enable aliases to be 'sudo' ed
-
-alias pwdc='pwd | tr -d "\n" | pbcopy'
-alias pwdc='pwd | tr -d "\n" | pbcopy'
-
-alias reload='source ~/.zshrc'
-
-# Reload the shell (i.e. invoke as login shell)
-alias relogin='exec $SHELL -l'
-
-# clean and cd
-alias c='cd ~ && clear'
 
 # Recursively delete `.DS_Store` files
 alias cleanup="find ${HOME} -name '*.DS_Store' -type f -ls -delete"
 
 # Remove broken symlinks
 alias clsym="find -L . -name . -o -type d -prune -o -type l -exec rm {} +"
-
-# ROT13-encode text. Works for decoding, too! ;)
-alias rot13='tr a-zA-Z n-za-mN-ZA-M'
-
-alias path='echo -e ${PATH//:/\\n}'
