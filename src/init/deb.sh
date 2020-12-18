76ac74a77cd4f6ff56d61b89eef630ffc59d9099
kabn3d <44823403+kabn3d@users.noreply.github.com>
2020-12-18 22:15:27 +0900

    Update deb.sh

--- src/init/deb.sh
+++ src/init/deb.sh
@@ -3,9 +3,6 @@
 sudo apt update
 sudo apt upgrade
 
-# install Guest Additions
-sudo apt install gcc make perl linux-headers-$(uname -r)
-
 yes | sudo apt install git
 yes | sudo apt install vim
 yes | sudo apt install zsh
